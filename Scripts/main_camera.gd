3ff5e0268da7c0afe5172d45b4d0c75884fe0b7a
tvstaticghost <ray.m.arnold@gmail.com>
2025-08-11 21:06:47 -0400

    Fixed camera rotate and added camera zoom functionality. Added object selection functionality

--- Scripts/main_camera.gd
+++ Scripts/main_camera.gd
@@ -1,48 +1,80 @@
 extends Camera3D
 
-# Script to pan camera around the scene
-var camera_rotation_amount: float = 0.002
-var max_pitch: float = 0.22
-var min_pitch: float = -0.25 
+var camera_rotation_amount: float = 0.2
+var camera_zoom_amount: float = 0.5
+
+var starting_camera_x: float
+var starting_camera_y: float
+var max_fov: float = 80.0
+var min_fov: float = 30.0
+#30.0 to 80.0 FOV
 
 var is_camera_moving: bool = false
 
-func play_camera_movement_sound():
-	$AudioStreamPlayer3D.play()
+func _ready() -> void:
+	starting_camera_x = rotation_degrees.x
+	starting_camera_y = rotation_degrees.y
+	print(rotation_degrees.x)
 	
-func stop_camera_movement_sound():
-	$AudioStreamPlayer3D.stop()
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("click"):
+		shoot_ray()
+	
+func shoot_ray():
+	var mouse_pos = get_viewport().get_mouse_position()
+	var ray_length = 1000
+	var from = project_ray_origin(mouse_pos)
+	var to = from + project_ray_normal(mouse_pos) * ray_length
+	var space = get_world_3d().direct_space_state
+	var ray_query = PhysicsRayQueryParameters3D.new()
+	ray_query.from = from
+	ray_query.to = to
+	var raycast_result = space.intersect_ray(ray_query)
+	
+	if !raycast_result.is_empty():
+		print(str(raycast_result.collider.name))
+		#Change this functionality to display in UI
 
 func _process(_delta: float) -> void:
 	is_camera_moving = false
-	# Tilt down
+	
+	# Look down
 	if Input.is_action_pressed("down"):
-		if rotation.x + camera_rotation_amount <= max_pitch:
-			rotation.x += camera_rotation_amount
+		if rotation_degrees.x - camera_rotation_amount >= starting_camera_x - 15.0:
+			rotation_degrees.x -= camera_rotation_amount
 			is_camera_moving = true
 
-	# Tilt up
+	# Look up
 	if Input.is_action_pressed("up"):
-		if rotation.x - camera_rotation_amount >= min_pitch:
-			rotation.x -= camera_rotation_amount
+		if rotation_degrees.x + camera_rotation_amount <= starting_camera_x + 15.0:
+			rotation_degrees.x += camera_rotation_amount
 			is_camera_moving = true
 			
-	# Tilt left
+	# Turn left
 	if Input.is_action_pressed("left"):
-		if rotation.y - camera_rotation_amount >= min_pitch:
-			rotation.y -= camera_rotation_amount
+		if rotation_degrees.y + camera_rotation_amount <= starting_camera_y + 15.0:
+			rotation_degrees.y += camera_rotation_amount
 			is_camera_moving = true
 			
-	# Tilt right
+	# Turn right
 	if Input.is_action_pressed("right"):
-		if rotation.y + camera_rotation_amount <= max_pitch:
-			rotation.y += camera_rotation_amount
+		if rotation_degrees.y - camera_rotation_amount >= starting_camera_y - 15.0:
+			rotation_degrees.y -= camera_rotation_amount
 			is_camera_moving = true
+			
+	if Input.is_action_pressed("zoom_in"):
+		print("Zoom in")
+		if fov - camera_zoom_amount >= min_fov:
+			fov -= camera_zoom_amount
+	
+	if Input.is_action_pressed("zoom_out"):
+		print("zoom out")
+		if fov + camera_zoom_amount <= max_fov:
+			fov += camera_zoom_amount
+		
 			
 	if not Input.is_anything_pressed():
 		is_camera_moving = false
-		
-	print(is_camera_moving)
 		
 	if is_camera_moving:
 		if not $AudioStreamPlayer3D.playing:
@@ -50,3 +82,9 @@ func _process(_delta: float) -> void:
 	else:
 		if $AudioStreamPlayer3D.playing:
 			stop_camera_movement_sound()
+
+func play_camera_movement_sound():
+	$AudioStreamPlayer3D.play()
+	
+func stop_camera_movement_sound():
+	$AudioStreamPlayer3D.stop()
